16454799e9c21342e3d989b2db0aa406bf46c110
mririi <wassim.mriri@gmail.com>
2023-05-14 21:35:45 +0100

    Fill manually done

--- src/screens/Tabs/MenuItems/TransactionDetails.js
+++ src/screens/Tabs/MenuItems/TransactionDetails.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { View } from 'react-native'
 import CustomText from '../../../component/CustomText'
 import colors from '../../../constants/colors'
+import CustomButton from '../../../component/CustomButton'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
-const TransactionDetails = (route) => {
-    console.log(route.params.data)
+const TransactionDetails = ({route}) => {
+    const [merchantName,setMerchantName]=useState("")
+    useEffect(()=>{
+      const data=route.params.data
+      setMerchantName(data.fields.MerchantName.text??'No Merchant Name')
+      
+    },[route])
   return (
-    <View style={{backgroundColor:colors.backgroundColor}}>
-        <CustomText>TransactionDetails</CustomText>
+    <View style={{backgroundColor:colors.background,flex:1,alignItems:"center",justifyContent:"center"}}>
+        <CustomText title="true">{merchantName}</CustomText>
+        
+        <CustomButton title="Save"/>
     </View>
   )
 }
